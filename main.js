6ce6f93426d7273dc0e0f15dce595325482a35fe
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-27 17:10:14 +0530

    loading main screen

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const electron = require("electron");
-
+const url = require("url");
+const path = require("path");
 const { app, BrowserWindow, Menu } = electron;
 
 let mainwindow;
@@ -7,7 +8,13 @@ let addWindow;
 
 app.on("ready", () => {
   mainwindow = new BrowserWindow();
-  mainwindow.loadURL();
+  mainwindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "/screens/index.htm"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
 });
 
 app.on("close", () => {
